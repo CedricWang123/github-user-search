d1ddc6f8af0c35ab032beae4e561e726a3ec38d1
cedric <464231692@qq.com>
2020-01-12 16:00:23 -0500

    Updates

---
+++ precache-manifest.4e2a4c3ea7aa462874800c056e68f60d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "256cf1583fa598c06f0b2193a8e624c1",
+    "url": "/github-user-search/index.html"
+  },
+  {
+    "revision": "848e1b7e507b76f63da5",
+    "url": "/github-user-search/static/css/main.a9a0eb7b.chunk.css"
+  },
+  {
+    "revision": "f2a47a2a2ce4574a0bd5",
+    "url": "/github-user-search/static/js/2.0effb713.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/github-user-search/static/js/2.0effb713.chunk.js.LICENSE"
+  },
+  {
+    "revision": "848e1b7e507b76f63da5",
+    "url": "/github-user-search/static/js/main.c83dec22.chunk.js"
+  },
+  {
+    "revision": "ddf4c0a81eb9f023c87b",
+    "url": "/github-user-search/static/js/runtime-main.593687b5.js"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/github-user-search/static/media/github_badge.0ba2aa20.png"
+  }
+]);
